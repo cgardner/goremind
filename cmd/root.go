c4b4fbf8edd100f83b04e591d22e4b53cab11596
Craig Gardner <craig.s.gardner@gmail.com>
2021-01-02 11:57:19 -0800

    feat: reading the remind filetypes a bit better

--- cmd/root.go
+++ cmd/root.go
@@ -1,11 +1,15 @@
 package cmd
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+
+	"github.com/cgardner/goremind/reminder"
+
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
-	"os"
 )
 
 var (
@@ -17,7 +21,28 @@ var rootCmd = &cobra.Command{
 	Use:   "goremind",
 	Short: "Modern implementation of remind",
 	Run: func(cmd *cobra.Command, args []string) {
+		log.Debug().Str("configFile", configFile).Msg("Starting execution")
+		file, err := os.Open(configFile)
+		if err != nil {
+			log.Fatal().Err(err).Str("configFile", configFile).Msg("Error Reading Config File")
+		}
+		defer file.Close()
 
+		scanner := bufio.NewScanner(file)
+		// lines := []string{}
+		for scanner.Scan() {
+			line := scanner.Text()
+			if reminder.IsComment(line) {
+				continue
+			}
+			if reminder.IsReminder(line) {
+				reminder.NewReminder(line)
+			}
+		}
+		err = scanner.Err()
+		if err != nil {
+			log.Fatal().Msg("Failed to read the config file")
+		}
 	},
 }
 
@@ -40,16 +65,8 @@ func init() {
 func initConfig() {
 	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
 
-	zerolog.SetGlobalLevel(zerolog.FatalLevel)
+	zerolog.SetGlobalLevel(zerolog.InfoLevel)
 	if verbose {
 		zerolog.SetGlobalLevel(zerolog.DebugLevel)
 	}
-
-	if configFile != "" {
-		log.Debug().
-			Str("configFile", configFile).
-			Msg("Loading Configuration File")
-		fmt.Println("reading config file", configFile)
-	}
-
 }
